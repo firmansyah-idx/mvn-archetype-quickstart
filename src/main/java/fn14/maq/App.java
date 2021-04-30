3ee8f844a7d1ecd223e4695e2c87bee063f8d7b1
Muhammad Firmansyah <63793814+FN14@users.noreply.github.com>
2021-05-01 00:00:34 +0700

    Initial Project Maven Quickstart

---
+++ src/main/java/fn14/maq/App.java
@@ -0,0 +1,13 @@
+package fn14.maq;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
